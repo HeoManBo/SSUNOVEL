66087da7370dae70c3a74a0fe966d866cdd9f435
HeoManBo <iovesssr__@naver.com>
2023-04-10 15:31:20 +0900

    초기 설정

---
+++ NovelForm/src/main/java/NovelForm/NovelForm/NovelFormApplication.java
@@ -0,0 +1,13 @@
+package NovelForm.NovelForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelFormApplication.class, args);
+	}
+
+}
---
+++ NovelForm/src/test/java/NovelForm/NovelForm/NovelFormApplicationTests.java
@@ -0,0 +1,13 @@
+package NovelForm.NovelForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
