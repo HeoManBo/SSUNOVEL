2edf64b2a88b6d61daf735f32879c483df89858d
jinsoo Kim <jinsoo335@naver.com>
2023-04-27 11:05:38 +0900

    [추가] 로그인 요청 DTO 생성
    누락되어 있던 로그인 요청 DTO를 올립니다.

---
+++ NovelForm/src/main/java/NovelForm/NovelForm/domain/member/dto/LoginMemberRequest.java
@@ -0,0 +1,27 @@
+package NovelForm.NovelForm.domain.member.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginMemberRequest {
+    @Schema(description = "이메일", defaultValue = "test@naver.com")
+    @NotEmpty
+    @Email
+    private String email;
+
+    @Schema(description = "비밀번호", defaultValue = "password123")
+    @NotEmpty
+    @Size(max = 20, min = 6)
+    private String password;
+
+}
