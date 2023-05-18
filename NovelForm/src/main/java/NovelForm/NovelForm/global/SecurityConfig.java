2a7eb9bbd75dedeb95d91796d42490fbe0feda2a
jinsoo Kim <jinsoo335@naver.com>
2023-05-18 18:27:16 +0900

    [추가] CORS 허용
    - 도메인이 다른 프론트 쪽에서 POST 등의 요청을 보낼 수 있게 처리

--- NovelForm/src/main/java/NovelForm/NovelForm/global/SecurityConfig.java
+++ NovelForm/src/main/java/NovelForm/NovelForm/global/SecurityConfig.java
@@ -18,8 +18,11 @@ public class SecurityConfig{
     // filterChain을 설정해두면, 컨트롤러로 정상적으로 요청이 들어온다.
     // 다만, disable() 처리를 한 부분이 문제가 될 수 있다.
     // 아마 쿠키를 다루는 부분에서 문제가 될 수 있으니 나중에 문제가 난다면 여기를 확인하자.
+
+    // cors 허용 -> 브라우저에서 다른 도메인에서 내 도메인으로 요청을 전달 할 수 있다.
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors();    // cors 허용
         http.csrf().disable();
         return http.build();
     }
