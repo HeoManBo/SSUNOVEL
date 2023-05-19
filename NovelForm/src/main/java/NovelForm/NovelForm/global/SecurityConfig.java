e33e77808da3b853026d82d63bc9efd8ce4a3fa1
jinsoo Kim <jinsoo335@naver.com>
2023-05-19 11:22:19 +0900

    [수정] CORS 허용 시도2 - CorsRegistry에 매핑 정보 추가
    - Configure로 CORS 허용을 시도

--- NovelForm/src/main/java/NovelForm/NovelForm/global/SecurityConfig.java
+++ NovelForm/src/main/java/NovelForm/NovelForm/global/SecurityConfig.java
@@ -31,4 +31,6 @@ public class SecurityConfig{
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
 }
---
+++ NovelForm/src/main/java/NovelForm/NovelForm/global/WebConfig.java
@@ -0,0 +1,18 @@
+package NovelForm.NovelForm.global;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://novelforum.shop")
+                .allowedMethods("*")
+                .allowCredentials(false)
+                .maxAge(3000);
+    }
+}
