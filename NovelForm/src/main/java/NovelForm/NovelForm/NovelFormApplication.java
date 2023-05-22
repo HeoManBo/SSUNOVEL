f59af3f08579c1aad5846a089abdeb942cf1e871
jinsoo Kim <jinsoo335@naver.com>
2023-05-22 12:24:07 +0900

    [추가] 프론트 빌드 파일
    - static 폴더에 추가

--- NovelForm/src/main/java/NovelForm/NovelForm/NovelFormApplication.java
+++ NovelForm/src/main/java/NovelForm/NovelForm/NovelFormApplication.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @EnableJpaAuditing
@@ -13,6 +14,12 @@ public class NovelFormApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NovelFormApplication.class, args);
+	}
+
+
+	@GetMapping("/")
+	public String index(){
+		return "index.html";
 	}
 
 }
